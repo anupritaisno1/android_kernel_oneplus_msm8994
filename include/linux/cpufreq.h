55ea1ede6e2d2d1b1713a3689ebf8ecaf3e0b178
andip71 <andreasp@gmx.de>
2015-10-05 07:01:30 +0200

    cpufreq: Add impulse governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -443,6 +443,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
